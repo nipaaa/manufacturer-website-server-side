2aa6a30ea8287dc4d81bfb4cc26feb4dfa524b6e
Nipa <akternipa941@gmail.com>
2022-05-28 00:37:47 +0600

    admin create

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 //middleware
